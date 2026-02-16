a5caec1d0fd6f362c2f33602661c1b96fe968dd4
b3nj4m1n-5chm1d7 <b3njamin.5chm1d7@gmail.com>
2026-02-16 13:03:01 -0300

    Pantalla de Inicio

--- projects/MiProyecto/Partida.cpp
+++ projects/MiProyecto/Partida.cpp
@@ -5,19 +5,50 @@
 Jugador player;
 Pantalla p;
 
+//Se establece una etapa de inicio (pantalla de inicio)
 void comienzo(){
-	cout<<"hola";
+	
+	//Se establece una distancia del lado superior hasta el texto
+	for (int i = 0;  i < 6; i ++){
+		cout <<endl;
+	}
+	
+	//Se presenta el titulo e instruciones del juego
+	cout << "                                                ============================="<<endl;
+	cout << "                                                       Space Invaders        "<<endl<<endl;
+	cout << "                                                     Intro Programacion        "<<endl;
+	cout << "                                                      Benjamin Schmidt        "<<endl;
+	cout << "                                                ============================="<<endl<<endl;
+	cout << "                                                       Instrucciones:        "<<endl;
+	cout << "                            Movimiento: presione la tecla A para mover la nave hacia la izquierda,        "<<endl;
+	cout << "                                        presione la tecla D para mover hacia la derecha        "<<endl;
+	cout << "                                   Disparo: presione la tecla ESPACIO para accionar el disparo        "<<endl<<endl;
+	cout << "                                        Gana si todas las naves enemigas son destruidas        "<<endl;
+	cout << "                                  Pierde si queda sin vidas al alcanzarlo cualquier disparo enemigo,        "<<endl;
+	cout << "                                         o si los enemigos alcanzan la misma linea que el jugados        "<<endl<<endl<<endl;
+	cout << "                                           Presiona cualquier tecla para comenzar..."<<endl;
+	
+	//Se espera a que el jugador presione alguna tecla
+	getch();
+	//Se limpia la pantalla
+	system("cls");
 }
 
+//Se establece una etapa donde se cargan los contenidos del juego
+void andar(){
+	p.Interfaz();
+	player.Personaje();
+}
+
+//Se carga una "etapa" de Victoria
 void victoria(){
-	cout<<"Ganaste";
+	cout<<endl<<"Ganaste";
 }
 	
 Partida::inicio() {
 	
 	comienzo();
-	p.Interfaz();
-	player.Personaje();
+	andar();
 	victoria();
 }
 
