5af23f833c379ec75f993a0297ccff604c695513
b3nj4m1n-5chm1d7 <b3njamin.5chm1d7@gmail.com>
2026-02-17 17:38:33 -0300

    Movimiento de un enemigo

--- projects/MiProyecto/Enemigos.cpp
+++ projects/MiProyecto/Enemigos.cpp
@@ -14,7 +14,7 @@ Enemigos::Enemigos(){
 	alt = 3;
 	dir = 2;
 	
-	velocidad = 8;
+	velocidad = 5;
 	
 	putchxy(pos,alt,'W');
 	
@@ -28,6 +28,17 @@ void Enemigos::mover() {
 	
 		borrarEnm(pos,alt);
 		pos+= dir;
+		
+		if (pos >= 100){
+			pos = 100;
+			dir = -2;
+			alt++;
+		}
+		if (pos<= 20){
+			pos = 20;
+			dir = 2;
+			alt++;
+		}
 		drawEnm(pos,alt);
 	
 		tempoEnem = clock();
